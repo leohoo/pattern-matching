4cd07a42b202ef5abf844d5485dc872f298b1f70
Liu Wei <wei@liuwei-no-mac-mini-2.local>
2016-09-27 02:31:28 +0900

    Initial Commit

---
+++ PatMatch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PatMatch
+//
+//  Created by Liu Wei on 9/27/16.
+//  Copyright © 2016 Liu Wei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
